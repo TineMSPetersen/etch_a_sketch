4ddf865bd25155354b918eba675965f766ad03cf
Tine <111304307+TineMSPetersen@users.noreply.github.com>
2022-09-01 15:30:52 +0200

    Generate the divs
    Creating the loop function needed to generate the divs

---
+++ script.js
@@ -0,0 +1,12 @@
+// Selecting the container div
+const container = document.querySelector(".container");
+
+// Creating the grid Item variable to be used in the loop
+let gridItem;
+
+// Loop creates the div, gives it the correct classname and then append it to container
+for (let i = 0; i < 256; i++) {
+    gridItem = document.createElement('div');
+    gridItem.className = "grid_item";
+    container.append(gridItem);
+}
