ceffc8cfda708d301cfc483e7da1adf9037fbc69
Tine <111304307+TineMSPetersen@users.noreply.github.com>
2022-09-01 19:21:22 +0200

    Add user prompt
    Prompts the user for how wide they want their canvas

--- script.js
+++ script.js
@@ -5,14 +5,22 @@ const container = document.querySelector(".container");
 // Creating the grid Item variable to be used in the loop
 let gridItem;
 
+function getGridAmount() {
+    const gridAmount = parseInt(prompt("How many fields wide do you want your canvas?"));
+    return gridAmount;
+}
+
+let gridAmount = getGridAmount();
+
 // Loop creates the div, gives it the correct classname and then append it to container
-for (let i = 0; i < 256; i++) {
+for (let i = 0; i < gridAmount * gridAmount; i++) {
     gridItem = document.createElement('div');
     gridItem.className = "grid_item";
     container.append(gridItem);
 }
 }
 
+// Function changing style of the grid items when you hover the mouse over them
 function gridHover() {
     const grid = document.querySelectorAll('.grid_item');
 
